c528c332ba21e987fc407d37f928f8d625680ac4
Meghan Beck <65236975+megbeck12@users.noreply.github.com>
2023-01-22 16:18:12 -0600

    adding more functionality to todo modal

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineClose } from 'react-icons/md';
 import styles from '../styles/modules/modal.module.scss';
 import Button from './Button';
 
 function TodoModal({ modalOpen, setModalOpen }) {
+    const [title, setTitle] = useState('');
+    const [status, setStatus] = useState('incomplete');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({title, status})
+    }
   return (
-    <div>
-    {modalOpen && (
+    modalOpen && (
     <div className={styles.wrapper}>
         <div className={styles.container}>
             <div className={styles.closeButton} onClick={() => setModalOpen(false)} onKeyDown={() => setModalOpen(false)} tabIndex={0} role="button">
                 <MdOutlineClose>
                 </MdOutlineClose>
             </div>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
                 <h1 className={styles.formTitle}>Add Task</h1>
                 <label htmlFor="title">
                     title
-                    <input type="text" id="title"></input>
+                    <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
                 </label>
                 <label htmlFor="status">
                     status
-                    <select name="status" id="status">
+                    <select name="status" id="status" value={status} onChange={(e) => setStatus(e.target.value)}>
                         <option value="incomplete">Incomplete</option>
                         <option value="complete">Complete</option>
                     </select>
@@ -30,15 +35,14 @@ function TodoModal({ modalOpen, setModalOpen }) {
                     <Button type="submit" variant="primary">
                         Add Task
                     </Button>
-                    <Button type="button" variant="secondary">
+                    <Button type="button" variant="secondary" onClick={() => setModalOpen(false)} onKeyDown={() => setModalOpen(false)}>
                         Cancel
                     </Button>
                 </div>
             </form>
         </div>
     </div>
-    )}
-    </div>
+    )
   )
 }
 
